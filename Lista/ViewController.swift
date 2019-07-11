7937938455353822d2348dd82e3750ea4d5cbf70
Henri <hloukonen@gmail.com>
2019-07-11 23:48:30 +0300

    Initial Commit

---
+++ Lista/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lista
+//
+//  Created by Henri Loukonen on 11/07/2019.
+//  Copyright © 2019 Henri Loukonen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
