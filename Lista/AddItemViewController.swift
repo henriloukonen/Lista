4c9c21065fe96bb25b5a45cb4a55312857dae0ef
Henri <hloukonen@gmail.com>
2019-07-16 21:54:31 +0300

    added amount for item

--- Lista/AddItemViewController.swift
+++ Lista/AddItemViewController.swift
@@ -10,38 +10,40 @@ import UIKit
 
 class AddItemViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet var addButton: UIBarButtonItem!
     @IBOutlet var newItemTextField: UITextField!
-
+    @IBOutlet var amountTextField: UITextField!
+    
     var itemName: String!
-
+    var amountOfItems: Int!
+    
+    let done: UIBarButtonItem = UIBarButtonItem(title: "Add", style: .done, target: self, action: #selector(doneButtonAction))
+    let amount: UIBarButtonItem = UIBarButtonItem(title: "Amount", style: .plain, target: self, action: #selector(addAmount))
+    let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+    let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let items = [amount, flexSpace, done]
     
     override func viewDidLoad() {
         title = "Add Item"
         navigationController?.navigationItem.largeTitleDisplayMode = .never
+        
+        newItemTextField.layer.cornerRadius = 3
+        amountTextField.layer.cornerRadius = 6
+        
         newItemTextField.becomeFirstResponder()
-        addDoneButtonOnKeyboard()
-    }
-    
-    @IBAction func closeVC(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-        newItemTextField.resignFirstResponder()
         
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let text = newItemTextField.text, !text.isEmpty else {
-            return
-        }
-        itemName = text
-        newItemTextField.resignFirstResponder()
+        done.isEnabled = false
         
+        doneToolbar.barStyle = .default
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        newItemTextField.inputAccessoryView = doneToolbar
+        amountTextField.inputAccessoryView = doneToolbar
     }
     
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         newItemTextField.text = ""
-        addButton.isEnabled = false
-        
+        done.isEnabled = false
         return false
     }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -51,30 +53,36 @@ class AddItemViewController: UIViewController, UITextFieldDelegate {
         }
         let newText = oldText.replacingCharacters(in: stringRange, with: string)
         if newText.isEmpty {
-            addButton.isEnabled = false
-            
+            done.isEnabled = false
         } else {
-            addButton.isEnabled = true
+            done.isEnabled = true
         }
+        
+        
         return true
+        
+        
+    }
+
+    @IBAction func closeVC(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        newItemTextField.resignFirstResponder()
+        amountTextField.resignFirstResponder()
     }
     
-    func addDoneButtonOnKeyboard()
-    {
-        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
-        doneToolbar.barStyle = .default
-        
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let done: UIBarButtonItem = UIBarButtonItem(title: "Add", style: .done, target: self, action: #selector(doneButtonAction))
-        
-        let items = [flexSpace, done]
-        doneToolbar.items = items
-        doneToolbar.sizeToFit()
-        
-        newItemTextField.inputAccessoryView = doneToolbar
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let text = newItemTextField.text, !text.isEmpty else {
+            return
+        }
+        itemName = text
     }
     
+    @objc func addAmount() {
+        amountTextField.becomeFirstResponder()
+    }
     @objc func doneButtonAction() {
         performSegue(withIdentifier: "unwindAddItemVC", sender: nil)
+        newItemTextField.resignFirstResponder()
+        amountTextField.resignFirstResponder()
     }
 }
