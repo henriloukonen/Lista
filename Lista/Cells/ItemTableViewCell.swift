608d17760643ab88dd0b81c1e6c3d97d9d2affd8
Henri <hloukonen@gmail.com>
2019-07-23 18:12:31 +0300

    Styled cell that are marked as done

--- Lista/Cells/ItemTableViewCell.swift
+++ Lista/Cells/ItemTableViewCell.swift
@@ -10,24 +10,34 @@ import UIKit
 
 class ItemTableViewCell: UITableViewCell {
     @IBOutlet var amountLabel: UILabel!
-    @IBOutlet var itemLabel: UILabel!
+    @IBOutlet var itemLabel: UILabel! 
     
     func configureCell(from entity: List) {
         itemLabel?.text = entity.item
         amountLabel?.text = (entity.amount<1) ? nil : String(entity.amount)
 
         //Style the cell
+        if entity.isDone {
+            itemLabel?.textColor = Color.lightGray
+            itemLabel?.alpha = 0.4
+            itemLabel?.transform = CGAffineTransform(translationX: 25, y: 0)
+            itemLabel?.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        } else {
+            itemLabel?.textColor = nil
+            itemLabel?.alpha = 1
+            itemLabel?.transform = .identity
+            itemLabel?.transform = .identity
+        }
         
         _ = SeparatorStyle.none
         _ = layer.borderWidth = 2
         _ = layer.borderColor = UIColor.white.cgColor
     }
-    func configureSelectedCell() {
-        itemLabel?.alpha = 0.6
-        UIView.animate(withDuration: 2, delay: 1, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: [], animations: {
-            self.itemLabel?.transform =  CGAffineTransform(translationX: 20, y: 0)
-            })
-    }
+//    func animateMarking() {
+//        UIView.animate(withDuration: 1, delay: 0.3, options: [], animations: {
+//            
+//        })
+//    }
 }
 
 
